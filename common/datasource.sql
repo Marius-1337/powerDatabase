dcb416f70788145623f66a3b4d860ecba183f2e7
Marius Kroon <mariuskroon@Large-Hadron-Collider.local>
2024-07-23 13:26:27 +0200

    datasource upate

--- common/datasource.sql
+++ common/datasource.sql
@@ -26,6 +26,5 @@ CREATE TABLE IF NOT EXISTS common.datasource (
 );
 COMMIT;
 
-
 INSERT INTO common.datasource ("name", fullname, url, "owner", lastupdate) VALUES('ENTSO-e', 'ENTSO-e', 'entsoe.eu', 'Marius', '2024-05-07 00:00:00.000');
 INSERT INTO common.datasource ("name", fullname, url, "owner", lastupdate) VALUES('Wattsight', 'Wattsight, also known as Volue', 'https://api.volueinsight.com', 'Marius', '2024-07-23 00:00:00.000');
