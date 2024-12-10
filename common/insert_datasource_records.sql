e724f38dad5d452cb33e9db30d2f262189bea352
Marius Kroon <marius@shindogu.com>
2024-12-10 08:10:56 +0100

    minor changes

--- common/insert_datasource_records.sql
+++ common/insert_datasource_records.sql
@@ -1,5 +1,4 @@
 
-INSERT INTO common.datasource ("name", fullname, url, "owner", lastupdate) VALUES('ENTSO-e', 'ENTSO-e', 'entsoe.eu', 'Marius', '2024-05-07 00:00:00.000');
-INSERT INTO common.datasource ("name", fullname, url, "owner", lastupdate) VALUES('Wattsight', 'Wattsight, also known as Volue', 'https://api.volueinsight.com', 'Marius', '2024-07-23 00:00:00.000');
-INSERT INTO common.datasource ("name", fullname, url, "owner", lastupdate) VALUES('Eneco', 'Eneco Internal source', 'eneco.nl', 'Marius', '2024-09-30 00:00:00.000');
-INSERT INTO common.datasource ("name", fullname, url, "owner", lastupdate) VALUES('OpenMeteo', 'OpenMeteo', 'https://www.openmeteo.nl', 'Marius', '2024-09-23 00:00:00.000');
+INSERT INTO common.datasource ("name", fullname, url, "owner", lastupdate) VALUES('ENTSO-e', 'ENTSO-e', 'https://newtransparency.entsoe.eu', 'Marius', '2024-11-07 00:00:00.000');
+INSERT INTO common.datasource ("name", fullname, url, "owner", lastupdate) VALUES('Wattsight', 'Wattsight, also known as Volue', 'https://api.volueinsight.com', 'Marius', '2024-11-23 00:00:00.000');
+INSERT INTO common.datasource ("name", fullname, url, "owner", lastupdate) VALUES('OpenMeteo', 'OpenMeteo', 'https://www.openmeteo.nl', 'Marius', '2024-11-23 00:00:00.000');
